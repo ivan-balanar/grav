ce50e2fef7d6aa3e8d8ccf430ed8f1c4359ba440
ivan_bal <semenov.siberian@gmail.com>
2020-07-06 11:32:59 +0700

    add testimonials with carousel

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 $(function() {
-	$('.owl-carousel').owlCarousel({
+	$('.works-carousel').owlCarousel({
 		loop:true,
 		margin:30,
+		nav:true,
+		dots: false,
+		navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+		startPosition: 1,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+});
+
+$(function() {
+	$('.contributor-carousel').owlCarousel({
+		loop:true,
+		margin:0,
 		nav:true,
 		dots: false,
 		navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
